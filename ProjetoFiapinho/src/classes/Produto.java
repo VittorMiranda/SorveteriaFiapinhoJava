d045ee8100d0e09a37122279e7e33abac6598491
VittorMiranda <vitorh.472975@gmail.com>
2022-05-22 14:47:47 -0300

    Update Produto.java

--- ProjetoFiapinho/src/classes/Produto.java
+++ ProjetoFiapinho/src/classes/Produto.java
@@ -3,25 +3,26 @@ package classes;
 import java.io.Serializable;
 
 public class Produto implements Serializable {
-protected String validade,Nome;
-protected double preco,peso;
-protected int quantidade;
-
-//construtor
-public Produto(String validade, String nome, double preco, double peso, int quantidade) {
-	super();
-	this.validade = validade;
-	Nome = nome;
-	this.preco = preco;
-	this.peso = peso;
-	this.quantidade = quantidade;
-}
+	protected String validade,Nome,tipoproduto;
+	protected double preco,peso;
+	protected int quantidade;
 
 
-public String mostrarDados() {
-	return "\nNome: " + this.Nome + "\nValidade: " + this.validade + "\nPreço: " + this.preco + "\nPeso: " + this.peso + "\nQuantidade: " + this.quantidade;
-}
+	//construtor
+	public Produto(String validade, String nome, String tipoproduto, double preco, double peso, int quantidade) {
+		super();
+		this.validade = validade;
+		Nome = nome;
+		this.tipoproduto = tipoproduto;
+		this.preco = preco;
+		this.peso = peso;
+		this.quantidade = quantidade;
+	}
 
+
+	public String mostrarDados() {
+		return "\nNome: " + this.Nome + "\nValidade: " + this.validade + "\nPreï¿½o: " + this.preco + "\nPeso: " + this.peso + "\nQuantidade: " + this.quantidade + "\nTipo do produto: " + this.tipoproduto;
+	}
 
 
 }
