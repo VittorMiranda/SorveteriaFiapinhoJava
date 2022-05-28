c0ea888ee1a166e91b56cc95a995bf16036be895
VittorMiranda <105468710+VittorMiranda@users.noreply.github.com>
2022-05-27 22:04:34 -0300

    Update PainelPedido.java

--- ProjetoFiapinho/src/painel/PainelPedido.java
+++ ProjetoFiapinho/src/painel/PainelPedido.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -15,26 +16,34 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 import classes.Cliente;
+import classes.Nota;
+import classes.Pedido;
 
 public class PainelPedido extends JPanel {
-	private JLabel jlPedido, jlNomeCliente,jlDescricaoPedido, jlEnderecoCliente, jlTelefone, jlFormaPagamento, jlCPF;
-	private JTextField jtfNomeCliente, jtfEnderoCliente, jtfTelefone, jtfCPF;
+	private JLabel jlPedido, jlNomeCliente,jlDescricaoPedido, jlEnderecoCliente, jlTelefone, jlFormaPagamento, jlCPF, jlSorvete, jlQuantidade;
+	private JTextField jtfNomeCliente, jtfEnderoCliente, jtfTelefone, jtfCPF, jtfSorvete, jtfQuantidade;
 	private JButton jbPedir;
 	private JRadioButton jrbCartao, jrbVale, jrbDinheiro,jrbPix;
 	private ButtonGroup bgFormaPagamento;
 	private JTextArea jtaArea;
-	
-	private List<Cliente> clientes;
+	private Nota nota;
+	private List<Pedido> pedidos;
 	//construtor
-	public PainelPedido(List<Cliente> clientes) {
+	public PainelPedido(List<Pedido> pedidos) {
 		super();
-		this.clientes = clientes;
+		this.pedidos = pedidos;
 		setSize(400, 400);
 		setLayout(null);
 		setBackground(Color.lightGray); 
 		iniciarComponentes();
 		criarEventos();
 	}
+	
+	//metodos
+	
+
+	
+	
 
 	private void iniciarComponentes() {
 		jlPedido = new JLabel("Pedido");
@@ -56,6 +65,10 @@ public class PainelPedido extends JPanel {
 		jbPedir = new JButton("Pedir");
 		jlCPF = new JLabel("CPF:");
 		jtfCPF = new JTextField();
+		jlSorvete = new JLabel("Sorvete:");
+		jtfSorvete = new JTextField();
+		jlQuantidade = new JLabel("Quantidade:");
+		jtfQuantidade = new JTextField();
 		bgFormaPagamento = new ButtonGroup();
 		jlDescricaoPedido = new JLabel("Descrição do Pedido:");
 		jtaArea = new JTextArea();
@@ -72,6 +85,10 @@ public class PainelPedido extends JPanel {
 		add(jlPedido);
 		add(jlTelefone);
 		add(jtfTelefone);
+		add(jlSorvete);
+		add(jtfSorvete);
+		add(jlQuantidade);
+		add(jtfQuantidade);
 		add(jrbCartao);
 		add(jrbVale);
 		add(jrbDinheiro);
@@ -95,14 +112,18 @@ public class PainelPedido extends JPanel {
 		jtfTelefone.setBounds(205, 100, 120, 20);
 		jlEnderecoCliente.setBounds(50, 130, 100, 20);
 		jtfEnderoCliente.setBounds(50, 150, 280, 20);
-		jlDescricaoPedido.setBounds(15, 180, 120, 20);
-		jtaArea.setBounds(15, 200, 360, 50);
-		jlFormaPagamento.setBounds(120, 270, 150, 20);
-		jrbCartao.setBounds(10, 300, 70, 20);
-		jrbVale.setBounds(80, 300, 130, 20);
-		jrbDinheiro.setBounds(210, 300, 80, 20);
-		jrbPix.setBounds(290, 300, 50, 20);
-		jbPedir.setBounds(130, 350, 100, 40);
+		jlDescricaoPedido.setBounds(15, 220, 120, 20);
+		jtaArea.setBounds(15, 240, 360, 50);
+		jlQuantidade.setBounds(210, 170, 100, 25);
+		jtfQuantidade.setBounds(210, 190, 90, 20);
+		jlSorvete.setBounds(70, 170, 100, 25);
+		jtfSorvete.setBounds(70, 190, 110, 20);
+		jlFormaPagamento.setBounds(120, 300, 150, 20);
+		jrbCartao.setBounds(10, 330, 70, 20);
+		jrbVale.setBounds(80, 330, 130, 20);
+		jrbDinheiro.setBounds(210, 330, 80, 20);
+		jrbPix.setBounds(290, 330, 50, 20);
+		jbPedir.setBounds(130, 370, 100, 40);
 		
 		
 		
@@ -114,26 +135,35 @@ public class PainelPedido extends JPanel {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String nome, formapagamento = null, endereco, descricao, telefone, cpf;
+				String nome, formapagamento = null, endereco, descricao, telefone, cpf, sorvete = null;
+				double quantidade = 0;
 				
-				nome = jtfNomeCliente.getText();
-				endereco = jtfEnderoCliente.getText();
-				descricao = jtaArea.getText();
-				telefone = jtfTelefone.getText();
-				cpf = jtfCPF.getText();
+				if (!jtfCPF.getText().isEmpty() && !jtfEnderoCliente.getText().isEmpty() && !jtfNomeCliente.getText().isEmpty() && !jtfQuantidade.getText().isEmpty() && !jtfSorvete.getText().isEmpty() && !jtfTelefone.getText().isEmpty()) {
+					
+					nome = jtfNomeCliente.getText();
+					endereco = jtfEnderoCliente.getText();
+					descricao = jtaArea.getText();
+					telefone = jtfTelefone.getText();
+					cpf = jtfCPF.getText();
+					
+					if(jrbCartao.isSelected()) formapagamento = "Crédito";
+					if(jrbVale.isSelected()) formapagamento = "Vale alimentação";
+					if(jrbDinheiro.isSelected()) formapagamento = "Dinheiro";
+					if(jrbPix.isSelected()) formapagamento = "Pix";
+					
+					pedidos.add(new Pedido(nome, telefone, endereco, formapagamento, cpf, descricao, sorvete, quantidade));
+					
 				
-				if(jrbCartao.isSelected()) formapagamento = "Crédito";
-				if(jrbVale.isSelected()) formapagamento = "Vale alimentação";
-				if(jrbDinheiro.isSelected()) formapagamento = "Dinheiro";
-				if(jrbPix.isSelected()) formapagamento = "Pix";
-				
-				clientes.add(new Cliente(nome, telefone, endereco, formapagamento, cpf));
-				
+					
+				} else {
+					JOptionPane.showMessageDialog(null, "Prenncha todos os campos!", "Sorveteria", JOptionPane.ERROR_MESSAGE);
+				}
 				jtfCPF.setText("");
 				jtfEnderoCliente.setText("");
 				jtfNomeCliente.setText("");
 				jtfTelefone.setText("");
 				jtaArea.setText("");
+				
 				
 				
 				
