28ba49d82cf830e67e5b5ad93e6a92d321f6f12b
VittorMiranda <105468710+VittorMiranda@users.noreply.github.com>
2022-05-31 20:41:14 -0300

    Update Pedido.java

--- ProjetoFiapinho/src/classes/Pedido.java
+++ ProjetoFiapinho/src/classes/Pedido.java
@@ -7,28 +7,28 @@ public class Pedido extends Cliente implements Serializable{
 	private String descricao, sorvete;
 	private double  quantidade;
 	
-	private int numeroPedido = 0, resultado;
+	
 	
 	//construtor
-		
+	public Pedido(String nome, String telefone, String endereco, String formaPagamento, String cpf, String descricao,
+			String sorvete, double quantidade) {
+		super(nome, telefone, endereco, formaPagamento, cpf);
+		this.descricao = descricao;
+		this.sorvete = sorvete;
+		this.quantidade = quantidade;
+	}
 
 
 
 //metodos
 public void numeropedido() {
-	
+	int numeroPedido = 0, resultado;
 		for (int i = 0; i <= 10000000; i++) {
 			resultado = numeroPedido + i++;
 		}
 		}
 
-public Pedido(String nome, String telefone, String endereco, String formaPagamento, String cpf, String descricao,
-		String sorvete, double quantidade) {
-	super(nome, telefone, endereco, formaPagamento, cpf);
-	this.descricao = descricao;
-	this.sorvete = sorvete;
-	this.quantidade = quantidade;
-}
+
 
 public double totalM() {
 	return quantidade*30;
@@ -39,7 +39,7 @@ public String mostrarDados() {
 }
 
 public String mostrarDadosPedido() {
-	return "\nNº Pedido" + resultado + mostrarDadosNota() + "\nSorvete: " + this.sorvete + "\nQTD: " + this.quantidade + "Kg"+ "\nTotal: "+totalM() + "R$";
+	return mostrarDadosNota() + "\nSorvete: " + this.sorvete + "\nQTD: " + this.quantidade + "Kg"+ "\nTotal: "+totalM() + "R$";
 }
 
 
