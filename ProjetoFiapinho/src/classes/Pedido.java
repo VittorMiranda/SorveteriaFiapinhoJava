54081129a9f34d657b389b9d571a26a7a359496f
raphalindao <105462948+raphalindao@users.noreply.github.com>
2022-05-30 22:20:33 -0300

    Update Pedido.java

--- ProjetoFiapinho/src/classes/Pedido.java
+++ ProjetoFiapinho/src/classes/Pedido.java
@@ -1,45 +1,46 @@
 package classes;
+import java.io.Serializable;
+
 import classes.Sorveteria2;
 
-public class Pedido extends Cliente{
+public class Pedido extends Cliente implements Serializable{
 	private String descricao, sorvete;
 	private double  quantidade;
-	private Cliente cliente;
-	private Sorveteria2 sorveteria2;
 	
+	private int numeroPedido = 0, resultado;
 	
 	//construtor
 		
-public Pedido(String nome, String telefone, String endereco, String formaPagamento, String cpf, String descricao,
-			String sorvete, double quantidade) {
-		super(nome, telefone, endereco, formaPagamento, cpf);
-		this.descricao = descricao;
-		this.sorvete = sorvete;
-		this.quantidade = quantidade;
-	}
+
 
 
 //metodos
 public void numeropedido() {
-	int numeroPedido = 0, resultado;
+	
 		for (int i = 0; i <= 10000000; i++) {
 			resultado = numeroPedido + i++;
 		}
 		}
+
+public Pedido(String nome, String telefone, String endereco, String formaPagamento, String cpf, String descricao,
+		String sorvete, double quantidade) {
+	super(nome, telefone, endereco, formaPagamento, cpf);
+	this.descricao = descricao;
+	this.sorvete = sorvete;
+	this.quantidade = quantidade;
+}
 
 public double totalM() {
 	return quantidade*30;
 }
 
 public String mostrarDados() {
-	return mostrarDadosNota() +"\nSorvete: " + this.sorvete + "QTD: " + this.quantidade + "\nDescrição: " + this.descricao;
+	return mostrarDadosNota() +"\nSorvete: " + this.sorvete + "\nQTD: " + this.quantidade + "\nDescrição: " + this.descricao;
 }
 
 public String mostrarDadosPedido() {
-	return "\nSorvete: " + this.sorvete + "QTD: " + this.quantidade;
+	return "\nNº Pedido" + resultado + mostrarDadosNota() + "\nSorvete: " + this.sorvete + "\nQTD: " + this.quantidade + "Kg"+ "\nTotal: "+totalM() + "R$";
 }
 
-public String mostrarDadosNota() {
-	return cliente.mostrarDadosNota() + mostrarDadosPedido() + "\nTotal" + totalM() + sorveteria2.mostrarDadosNota(); 
-}
+
 }
